cdd20da97659c67cf4db01f6191a8b6ac6ca256a
BrackListed <cameta8900@gmail.com>
2026-05-06 18:01:14 +0800

    Removed the placeholder once an item is added to the list. Cleared out console logs.

--- dist/index.js
+++ dist/index.js
@@ -3,14 +3,18 @@ let addTask = document.querySelector(".add-task");
 let userTasks = JSON.parse(localStorage.getItem("task-storage") || "[]");
 let todayTasks = document.querySelector("#today-container");
 let newTask = null;
+let taskPlaceholder = document.getElementById("task-placeholder");
+if (userTasks.length > 0) {
+    taskPlaceholder.remove();
+}
+else {
+}
 addTask === null || addTask === void 0 ? void 0 : addTask.addEventListener("click", function () {
     newTask = document.createElement("input");
     newTask.id = "user-tasks";
     todayTasks === null || todayTasks === void 0 ? void 0 : todayTasks.appendChild(newTask);
-    console.log('oi');
     taskCreation();
 });
-console.log(userTasks);
 userTasks.forEach((tasks) => {
     let listTask = document.createElement("li");
     listTask.textContent = tasks;
@@ -20,18 +24,17 @@ function taskCreation() {
     newTask === null || newTask === void 0 ? void 0 : newTask.addEventListener("keydown", function (e) {
         if (e.key === "Enter") {
             userTasks.push(newTask.value);
-            console.log(userTasks);
             storeTask();
             let newlyCreated = document.createElement("li");
             newlyCreated.textContent = newTask.value;
             todayTasks === null || todayTasks === void 0 ? void 0 : todayTasks.appendChild(newlyCreated);
             newTask.remove();
+            taskPlaceholder.remove();
         }
     });
     newTask === null || newTask === void 0 ? void 0 : newTask.addEventListener("blur", function () {
         newTask.value = "";
     });
-    //create an input and append it to the container thing
 }
 function storeTask() {
     localStorage.setItem("task-storage", JSON.stringify(userTasks));
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,21 @@ let addTask = document.querySelector<HTMLDivElement>(".add-task")
 let userTasks:string[] = JSON.parse(localStorage.getItem("task-storage") || "[]")
 let todayTasks = document.querySelector<HTMLDivElement>("#today-container")
 let newTask:HTMLInputElement | null = null
+let taskPlaceholder = document.getElementById("task-placeholder") as HTMLLIElement
+
+if(userTasks.length > 0){
+    taskPlaceholder.remove()
+} else{
+    
+}
 
 addTask?.addEventListener("click", function(){
     newTask = document.createElement("input")
     newTask.id = "user-tasks"
     todayTasks?.appendChild(newTask)
-    console.log('oi')
     taskCreation()
 })
 
-console.log(userTasks)
 
 userTasks.forEach((tasks) => {
     let listTask:HTMLLIElement = document.createElement("li")
@@ -24,19 +29,18 @@ function taskCreation(){
     newTask?.addEventListener("keydown", function(e: KeyboardEvent){
         if(e.key === "Enter"){
             userTasks.push(newTask!.value)
-            console.log(userTasks)
             storeTask()
             let newlyCreated:HTMLLIElement = document.createElement("li")
             newlyCreated.textContent = newTask!.value
             todayTasks?.appendChild(newlyCreated)
             newTask!.remove()
+            taskPlaceholder.remove()
         }
     })
 
     newTask?.addEventListener("blur", function(){
         newTask!.value = ""
     })
-    //create an input and append it to the container thing
 }
 
 function storeTask(){
